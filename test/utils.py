7afe71fe512c6d2632f578d2ed9d3c2e08413aa7
Antoine Lavenant <Antoine.Lavenant@ign.fr>
2023-12-14 09:39:15 +0100

    update test code

--- test/utils.py
+++ test/utils.py
@@ -2,9 +2,9 @@ import pdal
 import os
 import subprocess
 
-os.environ["PDAL_DRIVER_PATH"] = os.path.abspath('./install/lib')
-
 def pdal_has_plugin(name_filter):
+    os.environ["PDAL_DRIVER_PATH"] = os.path.abspath('./install/lib')
+    print("init pdal driver : ", os.environ["PDAL_DRIVER_PATH"])
     result = subprocess.run(['pdal', '--drivers'], stdout=subprocess.PIPE)
     if name_filter not in result.stdout.decode('utf-8'):
         raise ValueError("le script " + name_filter + " n'est pas visible")
