f4e40b48b7a7bd2c91234c1c9c5ff47bae7912ac
Antoine Lavenant <Antoine.Lavenant@ign.fr>
2024-05-17 10:20:12 +0200

    no more shapely + fix test

--- test/test_radius_search.py
+++ test/test_radius_search.py
@@ -2,8 +2,6 @@ import json
 import tempfile
 from test import utils
 
-from shapely.geometry import Point
-
 from random import *
 import numpy as np
 import pdal
@@ -14,19 +12,21 @@ import pytest
 
 def test_radius_search():
 
-    distance_radius = 1
+    filter = "filters.radius_search"
+    utils.pdal_has_plugin(filter)
 
-    #shapely is 2d library
+    distance_radius = 1
+    distance_cylinder = 0.5
+
     def distance2d(pt1, pt2):
-        return pt1.distance(pt2)
+        return sqrt( (pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2 )
     def distance3d(pt1, pt2):
-        return sqrt( (pt1.x-pt2.x)**2 + (pt1.y-pt2.y)**2 + (pt1.z-pt2.z)**2 )
+        return sqrt( (pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2 + (pt1[2]-pt2[2])**2 )
 
     pt_x = 1639825.15
     pt_y = 1454924.63
     pt_z = 7072.17
     pt_ini = (pt_x, pt_y, pt_z, 1)
-    pt1 = Point(pt_x, pt_y, pt_z)
 
     dtype = [('X', '<f8'), ('Y', '<f8'), ('Z', '<f8'), ('Classification', 'u1')]
 
@@ -35,88 +35,85 @@ def test_radius_search():
     nb_points = randint(10, 20)
     nb_points_take_2d = 0
     nb_points_take_3d = 0
-    nb_points_take_2d_above_bellow = 0
+    nb_points_take_2d_above_below = 0
 
     for i in range(nb_points):
         pti_x = pt_x + rand.uniform(-1.5, 1.5)
         pti_y = pt_y + rand.uniform(-1.5, 1.5)
-        pti_z = round(pt_z + rand.uniform(-1.5, 1.5),2) #pdal only takes 2 numbers precision
+
+        # pdal write takes 2 numbers precision (scale_z=0.01 and offset_z=0 by default)
+        pti_z = round(pt_z + rand.uniform(-1.5, 1.5), 2)
         pt_i = (pti_x, pti_y, pti_z, 2)
 
         arrays_pti = np.array([pt_i], dtype=dtype)
         arrays_pts = np.concatenate((arrays_pts, arrays_pti), axis=0)
 
-        pt_ig = Point(pti_x, pti_y, pti_z)
-        distance_i_2d = distance2d(pt1, pt_ig)
-        distance_i_3d = distance3d(pt1, pt_ig)
+        pt_ig = (pti_x, pti_y, pti_z)
+        distance_i_2d = distance2d(pt_ini, pt_ig)
+        distance_i_3d = distance3d(pt_ini, pt_ig)
 
         if distance_i_2d < distance_radius:
             nb_points_take_2d += 1
-            if abs(pti_z-pt_z)<0.5:
-                nb_points_take_2d_above_bellow += 1
+            if abs(pti_z-pt_z) < distance_cylinder:
+                nb_points_take_2d_above_below += 1
         if distance_i_3d < distance_radius:
             nb_points_take_3d += 1
 
-    las_tmp = tempfile.NamedTemporaryFile(suffix="_las_tmp.las").name
-    pipeline = pdal.Writer.las(filename=las_tmp).pipeline(arrays_pts)
-    pipeline.execute()
+    with tempfile.NamedTemporaryFile(suffix="_las_tmp.las") as las:
+        pipeline = pdal.Writer.las(filename=las.name).pipeline(arrays_pts)
+        pipeline.execute()
 
-    filter = "filters.radius_search"
-    utils.pdal_has_plugin(filter)
+        PIPELINE = [
+            {"type": "readers.las", "filename": las.name},
+            {
+                "type": "filters.ferry",
+                "dimensions": "=>SRS_DOMAIN"
+            },
+            {
+                "type": "filters.ferry",
+                "dimensions": "=>REF_DOMAIN"
+            },
+            {
+                "type": "filters.assign",
+                "value": [
+                    "SRS_DOMAIN = 1 WHERE Classification==2",
+                    "SRS_DOMAIN = 0 WHERE Classification!=2",
+                    "REF_DOMAIN = 1 WHERE Classification==1",
+                    "REF_DOMAIN = 0 WHERE Classification!=1",
+                ],
+            },
+            {
+                "type": filter,
+                "radius": "1.",
+                "src_domain": "SRS_DOMAIN",
+                "reference_domain": "REF_DOMAIN",
+                "output_name_attribute": "radius_2D",
+                "search_3d":False,
+            },
+            {
+                "type": filter,
+                "radius": "1.",
+                "src_domain": "SRS_DOMAIN",
+                "reference_domain": "REF_DOMAIN",
+                "output_name_attribute": "radius_2D_above_bellow",
+                "search_3d": False,
+                "search_2d_above": distance_cylinder,
+                "search_2d_bellow": distance_cylinder,
+            },
+            {
+                "type": filter,
+                "radius": "1.",
+                "src_domain": "SRS_DOMAIN",
+                "reference_domain": "REF_DOMAIN",
+                "output_name_attribute": "radius_3D",
+                "search_3d": True
+            }
+        ]
 
-    PIPELINE = [
-        {"type": "readers.las", "filename": las_tmp},
-        {
-            "type": "filters.ferry",
-            "dimensions": "=>SRS_DOMAIN"
-        },
-        {
-            "type": "filters.ferry",
-            "dimensions": "=>REF_DOMAIN"
-        },
-        {
-            "type": "filters.assign",
-            "value": [
-                "SRS_DOMAIN = 1 WHERE Classification==2",
-                "SRS_DOMAIN = 0 WHERE Classification!=2",
-                "REF_DOMAIN = 1 WHERE Classification==1",
-                "REF_DOMAIN = 0 WHERE Classification!=1",
-            ],
-        },
-        {
-            "type": filter,
-            "radius": "1.",
-            "src_domain": "SRS_DOMAIN",
-            "reference_domain": "REF_DOMAIN",
-            "output_name_attribute": "radius_2D",
-            "search_3d":False,
-        },
-        {
-            "type": filter,
-            "radius": "1.",
-            "src_domain": "SRS_DOMAIN",
-            "reference_domain": "REF_DOMAIN",
-            "output_name_attribute": "radius_2D_above_bellow",
-            "search_3d": False,
-            "search_2d_above": 0.5,
-            "search_2d_bellow": 0.5,
-        },
-        {
-            "type": filter,
-            "radius": "1.",
-            "src_domain": "SRS_DOMAIN",
-            "reference_domain": "REF_DOMAIN",
-            "output_name_attribute": "radius_3D",
-            "search_3d": True
-        }
-    ]
-
-    pipeline = pdal.Pipeline(json.dumps(PIPELINE))
-
-    # execute the pipeline
-    pipeline.execute()
-    arrays = pipeline.arrays
-    array = arrays[0]
+        pipeline = pdal.Pipeline(json.dumps(PIPELINE))
+        pipeline.execute()
+        arrays = pipeline.arrays
+        array = arrays[0]
 
     nb_pts_radius_2d = 0
     nb_pts_radius_3d = 0
@@ -131,4 +128,4 @@ def test_radius_search():
 
     assert nb_pts_radius_2d == nb_points_take_2d
     assert nb_pts_radius_3d == nb_points_take_3d
-    assert nb_pts_radius_2d_above_bellow == nb_points_take_2d_above_bellow
+    assert nb_pts_radius_2d_above_bellow == nb_points_take_2d_above_below
