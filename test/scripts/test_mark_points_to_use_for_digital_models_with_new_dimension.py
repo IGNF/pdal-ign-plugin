1d814651fa5d69bdc7be63b8989254c9416c5b5c
Lea Vauchier <lea.vauchier@ign.fr>
2024-06-06 17:05:00 +0200

    Add test for mark_points script

---
+++ test/scripts/test_mark_points_to_use_for_digital_models_with_new_dimension.py
@@ -0,0 +1,23 @@
+import tempfile
+
+import numpy as np
+import pdal
+
+from scripts.mark_points_to_use_for_digital_models_with_new_dimension import main
+
+
+def test_main():
+    ini_las = "test/data/4_6.las"
+    dsm_dimension = "dsm_marker"
+    dtm_dimension = "dtm_marker"
+    with tempfile.NamedTemporaryFile(suffix="_mark_points_output.las") as las_output:
+        main(ini_las, las_output.name, dsm_dimension, dtm_dimension, "", "")
+        pipeline = pdal.Pipeline()
+        pipeline |= pdal.Reader.las(las_output.name)
+        assert dsm_dimension in pipeline.quickinfo["readers.las"]["dimensions"].split(", ")
+        assert dtm_dimension in pipeline.quickinfo["readers.las"]["dimensions"].split(", ")
+
+        pipeline.execute()
+        arr = pipeline.arrays[0]
+        assert np.any(arr[dsm_dimension] == 1)
+        assert np.any(arr[dtm_dimension] == 1)
