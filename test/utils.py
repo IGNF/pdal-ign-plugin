48b92f1303d22007fdf741852912311179fd37fa
Lea Vauchier <lea.vauchier@ign.fr>
2024-06-06 16:34:03 +0200

    Enable radisAssignFilter with max2d_above or max2d_below = 0

--- test/utils.py
+++ test/utils.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 def pdal_has_plugin(name_filter):
-    print("init pdal driver : ", os.environ["PDAL_DRIVER_PATH"])
+    print("Initialize pdal driver : ", os.environ["PDAL_DRIVER_PATH"])
     result = subprocess.run(["pdal", "--drivers"], stdout=subprocess.PIPE)
     if name_filter not in result.stdout.decode("utf-8"):
-        raise ValueError("le script " + name_filter + " n'est pas visible")
+        raise ValueError("Filter " + name_filter + " not found by `pdal --drivers`.")
