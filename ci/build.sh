7ee047b5fd3776c216c9a753ea28f247bdd12fa7
Lea Vauchier <lea.vauchier@ign.fr>
2024-06-19 17:13:38 +0200

    Fix build script to build on Ubuntu

--- ci/build.sh
+++ ci/build.sh
@@ -1,20 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-FILE=~/anaconda3/etc/profile.d/conda.sh
-if [ -e ~/anaconda3/etc/profile.d/conda.sh ]
+FILE=${HOME}/anaconda3/etc/profile.d/conda.sh
+if [ -e ${HOME}/anaconda3/etc/profile.d/conda.sh ]
 then
-    source ~/anaconda3/etc/profile.d/conda.sh
+    source ${HOME}/anaconda3/etc/profile.d/conda.sh
 elif [ -e ~/miniconda3/etc/profile.d/conda.sh ]
 then
-    source ~/miniconda3/etc/profile.d/conda.sh
+    source ${HOME}/miniconda3/etc/profile.d/conda.sh
 elif [ -e /usr/share/miniconda/etc/profile.d/conda.sh ]
 then
     source /usr/share/miniconda/etc/profile.d/conda.sh
-elif [ -e ~/miniforge3/etc/profile.d/conda.sh ]
+elif [ -e ${HOME}/miniforge3/etc/profile.d/conda.sh ]
 then
-     source ~/miniforge3/etc/profile.d/conda.sh
+     source ${HOME}/miniforge3/etc/profile.d/conda.sh
 elif [[ -z "${CONDASH}" ]]; then
     echo ERROR: Failed to load conda.sh : ~/anaconda3/etc/profile.d/conda.sh or ~/miniforge3/etc/profile.d/conda.sh or env CONDASH
     exit 1 # terminate and indicate error
@@ -26,9 +26,9 @@ fi
 conda activate pdal_ign_plugin
 
 export CONDA_PREFIX=$CONDA_PREFIX
-echo conda is $CONDA_PREFIX 
+echo conda is $CONDA_PREFIX
 
-mkdir build
+mkdir -p build
 cd build
 cmake -G"Unix Makefiles" -DCONDA_PREFIX=$CONDA_PREFIX -DCMAKE_BUILD_TYPE=Release ../
 make install
